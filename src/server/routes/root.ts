20fc230ba498c502020a2f01ee3f11b7fa8bb0fa
John Roberts <roberts.john@gmail.com>
2024-10-21 20:29:16 -0700

    Initial setup completed

--- src/server/routes/root.ts
+++ src/server/routes/root.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (_request, response) => {
-  response.render("root", { title: "Jrob's site" });
+  response.render("root", { title: "Jrob's super site" });
 });
 
 export default router;
