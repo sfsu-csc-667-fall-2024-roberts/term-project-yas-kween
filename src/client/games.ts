efaeea4c4a276c810bd4cd402f7d21f044f60eb3
John Roberts <roberts.john@gmail.com>
2024-11-21 19:05:11 -0800

    wip

--- src/client/games.ts
+++ src/client/games.ts
@@ -1,12 +1,11 @@
+// window.socket.on("thing", console.log);
+
 document
   .querySelector<HTMLFormElement>("#blarg")!
   .addEventListener("submit", (event) => {
     event.preventDefault();
 
     const form = event.target as HTMLFormElement;
-    console.log({ form });
 
-    fetch(form.action, { method: "post" }).then((response) => {
-      console.log({ response });
-    });
+    fetch(form.action, { method: "post" });
   });
